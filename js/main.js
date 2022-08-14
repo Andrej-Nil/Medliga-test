05fec44a0443e61ff6dd2eca8dcb6ee585e3e635
AndrejNil <and.kucheroff@yandex.ru>
2022-08-14 16:40:13 +0500

    Описал логику сортировки, включая отрисовку товаров

---
+++ js/main.js
@@ -0,0 +1,634 @@
+class Service {
+  constructor() {
+  }
+  testData = {
+    products: [
+      {
+        id: 0,
+        title: 'Hitachi Arietta 850',
+        brand: './img/image/brand.png',
+        link: '#!',
+        price: { old: '10 000 000 ₽', current: '8 000 000 ₽' },
+        sale: '20%',
+        stickers: [
+          './img/icon/1.png',
+          './img/icon/2.png',
+          './img/icon/3.png'
+        ],
+        property: [
+          { label: 'Вид оборудования', value: 'УЗИ аппарат' },
+          { label: 'Производитель', value: 'Mindray' },
+          { label: 'Тип', value: 'Портативный' },
+          { label: 'Класс', value: 'Высокий' },
+        ],
+        img: [
+          './img/image/pic2.png',
+          './img/image/pic1.png',
+          './img/image/pic3.png',
+          './img/image/pic4.png',
+          './img/image/pic5.png',
+        ]
+      },
+
+      {
+        id: 1,
+        title: 'Ультразвуковая диагностическая система Mindray DC-60 Exp',
+        brand: './img/image/brand.png',
+        link: '#!',
+        price: { old: '7 000 000 ₽', current: 'от 1 990 580 ₽' },
+        sale: '20%',
+        stickers: [
+          './img/icon/1.png',
+          './img/icon/2.png'
+        ],
+        property: [
+          { label: 'Вид оборудования', value: 'УЗИ аппарат' },
+          { label: 'Производитель', value: 'Mindray' },
+          { label: 'Тип', value: 'Портативный' },
+          { label: 'Класс', value: 'Высокий' },
+        ],
+        img: [
+          './img/image/pic1.png',
+          './img/image/pic2.png',
+          './img/image/pic3.png',
+          './img/image/pic4.png',
+          './img/image/pic5.png',
+        ]
+      },
+
+      {
+        id: 2,
+        title: 'УЗИ аппарат Mindray M5 new ',
+        brand: './img/image/brand.png',
+        link: '#!',
+        price: { old: '11 000 000 ₽', current: '9 000 000 ₽' },
+        sale: '20%',
+        stickers: [
+          './img/icon/2.png',
+          './img/icon/1.png',
+          './img/icon/3.png'
+        ],
+        property: [
+          { label: 'Вид оборудования', value: 'УЗИ аппарат' },
+          { label: 'Производитель', value: 'Mindray' },
+          { label: 'Тип', value: 'Портативный' },
+          { label: 'Класс', value: 'Высокий' },
+        ],
+        img: [
+          './img/image/pic4.png',
+          './img/image/pic5.png',
+          './img/image/pic1.png',
+
+        ]
+      },
+
+      {
+        id: 3,
+        title: 'Hitachi Arietta 850',
+        brand: './img/image/brand.png',
+        link: '#!',
+        price: { old: '10 000 000 ₽', current: '8 000 000 ₽' },
+        sale: '20%',
+        stickers: [
+          './img/icon/1.png',
+          './img/icon/2.png',
+          './img/icon/3.png'
+        ],
+        property: [
+          { label: 'Вид оборудования', value: 'УЗИ аппарат' },
+          { label: 'Производитель', value: 'Mindray' },
+          { label: 'Тип', value: 'Портативный' },
+          { label: 'Класс', value: 'Высокий' },
+        ],
+        img: [
+          './img/image/pic2.png',
+          './img/image/pic1.png',
+          './img/image/pic3.png',
+          './img/image/pic4.png',
+          './img/image/pic5.png',
+        ]
+      },
+
+      {
+        id: 4,
+        title: 'Ультразвуковая диагностическая система Mindray DC-60 Exp',
+        brand: './img/image/brand.png',
+        link: '#!',
+        price: { old: '', current: '5 000 000 ₽' },
+        sale: '20%',
+        stickers: [
+          './img/icon/3.png',
+          './img/icon/2.png'
+        ],
+        property: [
+          { label: 'Вид оборудования', value: 'УЗИ аппарат' },
+          { label: 'Производитель', value: 'Mindray' },
+          { label: 'Тип', value: 'Портативный' },
+          { label: 'Класс', value: 'Высокий' },
+        ],
+        img: [
+          './img/image/pic1.png',
+          './img/image/pic2.png',
+          './img/image/pic4.png',
+          './img/image/pic5.png',
+        ]
+      },
+
+      {
+        id: 5,
+        title: 'УЗИ аппарат Mindray M5 new ',
+        brand: './img/image/brand.png',
+        link: '#!',
+        price: { old: '11 000 000 ₽', current: '9 000 000 ₽' },
+        sale: '20%',
+        stickers: [
+          './img/icon/2.png',
+          './img/icon/1.png',
+          './img/icon/3.png'
+        ],
+        property: [
+          { label: 'Вид оборудования', value: 'УЗИ аппарат' },
+          { label: 'Производитель', value: 'Mindray' },
+          { label: 'Тип', value: 'Портативный' },
+          { label: 'Класс', value: 'Высокий' },
+        ],
+        img: [
+          './img/image/pic4.png',
+          './img/image/pic5.png',
+          './img/image/pic1.png',
+          './img/image/pic2.png',
+          './img/image/pic3.png',
+
+        ]
+      },
+      {
+        id: 6,
+        title: 'УЗИ аппарат Mindray M5 new ',
+        brand: './img/image/brand.png',
+        link: '#!',
+        price: { old: '', current: '9 000 000 ₽' },
+        sale: '20%',
+        stickers: [
+          './img/icon/1.png',
+          './img/icon/3.png'
+        ],
+        property: [
+          { label: 'Вид оборудования', value: 'УЗИ аппарат' },
+          { label: 'Производитель', value: 'Mindray' },
+          { label: 'Класс', value: 'Высокий' },
+        ],
+        img: [
+          './img/image/pic4.png',
+          './img/image/pic5.png',
+          './img/image/pic2.png',
+          './img/image/pic3.png',
+
+        ]
+      },
+
+      {
+        id: 7,
+        title: 'Hitachi Arietta 850',
+        brand: './img/image/brand.png',
+        link: '#!',
+        price: { old: '10 000 000 ₽', current: '8 000 000 ₽' },
+        sale: '20%',
+        stickers: [
+          './img/icon/1.png',
+          './img/icon/2.png',
+          './img/icon/3.png'
+        ],
+        property: [
+          { label: 'Вид оборудования', value: 'УЗИ аппарат' },
+          { label: 'Производитель', value: 'Mindray' },
+          { label: 'Тип', value: 'Портативный' },
+          { label: 'Класс', value: 'Высокий' },
+        ],
+        img: [
+          './img/image/pic2.png',
+          './img/image/pic1.png',
+          './img/image/pic3.png',
+          './img/image/pic4.png',
+          './img/image/pic5.png',
+        ]
+      },
+
+      {
+        id: 8,
+        title: 'Ультразвуковая диагностическая система Mindray DC-60 Exp',
+        brand: './img/image/brand.png',
+        link: '#!',
+        price: { old: '7 000 000 ₽', current: '5 000 000 ₽' },
+        sale: '20%',
+        stickers: [
+          './img/icon/3.png',
+          './img/icon/1.png',
+          './img/icon/2.png'
+        ],
+        property: [
+          { label: 'Производитель', value: 'Mindray' },
+          { label: 'Тип', value: 'Портативный' },
+          { label: 'Класс', value: 'Высокий' },
+        ],
+        img: [
+          './img/image/pic1.png',
+          './img/image/pic2.png',
+          './img/image/pic3.png',
+        ]
+      },
+
+      {
+        id: 9,
+        title: 'УЗИ аппарат Mindray M5 new ',
+        brand: './img/image/brand.png',
+        link: '#!',
+        price: { old: '11 000 000 ₽', current: '9 000 000 ₽' },
+        stickers: [
+          './img/icon/2.png',
+        ],
+        property: [
+          { label: 'Вид оборудования', value: 'УЗИ аппарат' },
+          { label: 'Производитель', value: 'Mindray' },
+          { label: 'Тип', value: 'Портативный' },
+          { label: 'Класс', value: 'Высокий' },
+        ],
+        img: [
+          './img/image/pic4.png',
+          './img/image/pic5.png',
+          './img/image/pic1.png',
+
+          './img/image/pic3.png',
+
+        ]
+      },
+
+      {
+        id: 10,
+        title: 'Ультразвуковая диагностическая система Mindray DC-60 Exp',
+        brand: './img/image/brand.png',
+        link: '#!',
+        price: { old: '7 000 000 ₽', current: '5 000 000 ₽' },
+        sale: '20%',
+        stickers: [
+          './img/icon/3.png',
+          './img/icon/1.png',
+          './img/icon/2.png'
+        ],
+        property: [
+          { label: 'Вид оборудования', value: 'УЗИ аппарат' },
+          { label: 'Производитель', value: 'Mindray' },
+          { label: 'Тип', value: 'Портативный' },
+          { label: 'Класс', value: 'Высокий' },
+        ],
+        img: [
+          './img/image/pic1.png',
+          './img/image/pic2.png',
+          './img/image/pic3.png',
+          './img/image/pic4.png',
+          './img/image/pic5.png',
+        ]
+      },
+
+      {
+        id: 11,
+        title: 'УЗИ аппарат Mindray M5 new ',
+        brand: './img/image/brand.png',
+        link: '#!',
+        price: { old: '11 000 000 ₽', current: '9 000 000 ₽' },
+        sale: '20%',
+        stickers: [
+          './img/icon/2.png',
+          './img/icon/1.png',
+          './img/icon/3.png'
+        ],
+        property: [
+          { label: 'Вид оборудования', value: 'УЗИ аппарат' },
+          { label: 'Производитель', value: 'Mindray' },
+          { label: 'Тип', value: 'Портативный' },
+          { label: 'Класс', value: 'Высокий' },
+        ],
+        img: [
+          './img/image/pic4.png',
+          './img/image/pic5.png',
+          './img/image/pic1.png',
+          './img/image/pic2.png',
+          './img/image/pic3.png',
+
+        ]
+      }
+    ]
+  }
+
+
+  getData(data) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (Math.random() > 0.80) {
+          resolve('error');
+        } else {
+          resolve(this.testData[data]);
+        }
+      }, 700);
+    });
+  }
+}
+
+class Render {
+  constructor($parent = null) {
+    //this.$parent = $parent;
+    this.declForTotalFindedItem = ['', 'а', 'ов'];
+    //this.spinnerText = '';
+  }
+
+  renderProductListLoader = ($parent) => {
+    this._render($parent, this.getProductListLoaderHtml);
+  }
+  renderProductListError = ($parent) => {
+    this._render($parent, this.getProductListErrorHtml);
+  }
+
+  renderProductCards = ($parent, cards) => {
+    this._render($parent, this.getProductCardHtml, false, cards);
+  }
+
+  //Методы отресовки элементов
+
+
+  //разметка
+
+  getProductListLoaderHtml = () => {
+    return (/*html */`
+    <div class="product-loader">
+      <span class="product-loader__message">Загружаю</span>
+    </div>
+    `)
+  }
+  getProductListErrorHtml = () => {
+    return (/*html */`
+      <div class="product-error">
+        <div class="product-error__content">
+          <img class="product-error__icon" src="./img/icon/broken-robot.svg" alt="">
+          <span class="product-error__message">Упс! Что то пошло не так, перезагрузите сраницу!</span>
+        </div>
+      </div>
+    `)
+  }
+
+  getProductCardHtml = (card) => {
+
+    const { id, title, property, img, link, price, brand, sale, stickers } = card;
+    const propertyHtml = this.getListHtml(this.getPropertyHtml, property);
+    const picHtml = this.getListHtml(this.getProductPicHtml, img);
+    const dotsHtml = this.getListHtml(this.getProductdotsHtml, img);
+    const saleHtml = sale ? this.getProductSaleStickerHtml(sale) : '';
+    const stickersHtml = stickers ? this.getListHtml(this.getProductStickerHtml, stickers) : '';
+    return (/*html */`
+      <div data-id="${id}" class="product-card">
+        <div class="product-card__bg">
+          <ul class="property">
+            ${propertyHtml}
+          </ul>
+        </div>
+
+        <div class="product-card__inner">
+          <div class="product-card__preveiw">
+            <div class="product-card__slider">
+              ${picHtml}
+            </div>
+            <div class="product-card__dots">
+              ${dotsHtml}
+            </div>
+          </div>
+          <h2 class="product-card__title">
+            <a class="product-card__link" href="${link}">
+              ${title}
+            </a>
+          </h2>
+
+          <div class="product-card__price">
+            <p class="product-card__price-old">${price.old}</p>
+            <p class="product-card__price-new">${price.current}</p>
+          </div>
+
+          <div class="product-card__nav">
+            <a href="${link}" class="product-card__more">Подробнее</a>
+            <button class="product-card__btn btn">Получить КП</button>
+          </div>
+
+
+        </div>
+        <span class="product-card__brand sticker-brand">
+          <img class="sticker-brand__img" src="${brand}" alt="brand">
+        </span>
+
+        <div class="product-card__stickers">
+          ${saleHtml}
+          ${stickersHtml}
+        </div>
+
+        <div class="product-card__icon-btns">
+          <span class="icon-btn eye"></span>
+          <span class="icon-btn heart active"></span>
+          <span class="icon-btn graph"></span>
+        </div>
+
+    </div>
+    `
+    )
+
+  }
+
+  getPropertyHtml = (item) => {
+    const { label, value } = item
+    return (/*html */`
+    <li class="property__item">
+      <span class="property__label">${label}</span>
+      <span class="property__dots"></span>
+      <span class="property__value">${value}</span>
+    </li>
+    `
+    )
+  }
+
+  getProductPicHtml = (img, idx) => {
+    const cls = idx === 0 ? 'product-card__pic active' : 'product-card__pic'
+    return (/*html */`
+      <img class="${cls}" src="${img}" alt="product"/>
+    `
+    )
+  }
+
+  getProductdotsHtml = (img, idx) => {
+    const cls = idx === 0 ? 'product-card__dot active' : 'product-card__dot'
+    return (/*html */`
+     <span class="${cls}" class="product-card__dot"></span>
+    `
+    )
+  }
+
+  getProductSaleStickerHtml = (sizeSale) => {
+    return (/*html */`
+      <span class="product-card__sale sticker-sale">
+        скидка -${sizeSale}
+      </span>
+   `
+    )
+  }
+
+  getProductStickerHtml = (img) => {
+    return (/*html */`
+      <span class="product-card__sticker sticker">
+        <img class="sticker__pic" src="${img}" alt="">
+      </span>
+   `
+    )
+  }
+  getListHtml = (getHtmlFn, arr) => {
+    let list = '';
+    arr.forEach((item, idx) => {
+
+      list += getHtmlFn(item, idx);
+    })
+
+    return list;
+  }
+
+  //Общая функция отрисовки
+  _render = ($parent, getHtmlMarkup, argument = false, array = false, where = 'beforeend') => {
+    let markupAsStr = '';
+    if (!$parent) {
+      return;
+    }
+    if (array) {
+      array.forEach((item) => {
+        markupAsStr = markupAsStr + getHtmlMarkup(item);
+      })
+    }
+    if (!array) {
+      markupAsStr = getHtmlMarkup(argument);
+    }
+    $parent.insertAdjacentHTML(where, markupAsStr);
+  }
+
+  //Методы удаление элементов
+
+  clearParent = ($parent = this.$parent) => {
+    if (!$parent) {
+      return;
+    }
+    $parent.innerHTML = '';
+  }
+
+  delete = ($el) => {
+    if (!$el) {
+      return;
+    }
+    $el.remove()
+  }
+
+  getDeclOfNum(number, decl) {
+    const cases = [2, 0, 1, 1, 1, 2];
+    return decl[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+  }
+
+  getTextWithIllumination = (str, text) => {
+    const reg = new RegExp(`${str}`, 'gi')
+    const illuminationStr = /*html*/`<span
+    class="result-card__light">${str}</span>`
+    return text.replace(reg, illuminationStr);
+  }
+
+}
+
+
+
+class Sort {
+  constructor() {
+    this.init()
+  }
+
+  init = () => {
+    this.$sortItemList = document.querySelectorAll('[data-sort]');
+    this.$productList = document.querySelector('[data-product-list]');
+    this.listeners()
+  }
+  closeSortPopup = ($sort) => {
+    const $popup = $sort.querySelector('[data-sort-popup]');
+    const $arrow = $sort.querySelector('[data-arrow]');
+    $arrow.classList.remove('up');
+    $popup.classList.remove('open');
+    $sort.dataset.sort = 'close';
+  }
+  closeSortPopupAll = () => {
+    this.$sortItemList.forEach(($item => {
+      this.closeSortPopup($item);
+    }))
+
+  };
+  openSortPopup = ($sort) => {
+
+    const $popup = $sort.querySelector('[data-sort-popup]');
+    const $arrow = $sort.querySelector('[data-arrow]');
+
+    this.closeSortPopupAll();
+    if (!$popup) return;
+    $popup.classList.add('open');
+    $sort.dataset.sort = 'open';
+    $arrow.classList.add('up');
+  };
+  toggleSortPopup = ($sortHead) => {
+    const $sort = $sortHead.closest('[data-sort]');
+    if (!$sort) {
+      this.closeSortPopupAll();
+      return
+    }
+    if ($sort.dataset.sort === 'close') {
+      this.openSortPopup($sort);
+    } else {
+      this.closeSortPopupAll($sort);
+    }
+  }
+
+  changeSortValue = async ($input) => {
+    const value = $input.dataset.radio;
+    const $sort = $input.closest('[data-sort]');
+    const $value = $sort.querySelector('[data-value]');
+    $value.innerHTML = value;
+    this.closeSortPopup($sort);
+
+    render.renderProductListLoader(this.$productList)
+
+    const res = await service.getData('products');
+
+
+    if (res === 'error') {
+      render.clearParent(this.$productList);
+      this.$productList.innerHTML = '<div class="product-card"></div>'
+      render.renderProductListError(this.$productList);
+    } else {
+      render.clearParent(this.$productList);
+      render.renderProductCards(this.$productList, res)
+    }
+
+
+  }
+
+
+  inputHandler = (e) => {
+    this.changeSortValue(e.target);
+  }
+
+  clickHandler = (e) => {
+    this.toggleSortPopup(e.target);
+  }
+
+
+  listeners = () => {
+    document.addEventListener('click', this.clickHandler);
+    document.addEventListener('input', this.inputHandler);
+  }
+}
+
+const sort = new Sort();
+const service = new Service();
+const render = new Render();
